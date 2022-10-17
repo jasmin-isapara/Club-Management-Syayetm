96d03635d994f6c7758f955966be4847cde2c810
Nathan Riley <nathan@prodjex.com>
2022-10-17 12:58:11 +0530

    setuo admin template

---
+++ resources/js/Layouts/Partials/Footer-script.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+import pluinsBundle from "../../../../public/admin/plugins/global/plugins.bundle";
+import scriptBundle from "../../../../public/admin/js/scripts.bundle";
+import fullcalendarBundle from "../../../../public/admin/plugins/custom/fullcalendar/fullcalendar.bundle";
+import datatablesBundle from "../../../../public/admin/plugins/custom/datatables/datatables.bundle";
+import widgetsBundle from "../../../../public/admin/js/widgets.bundle";
+import widgets from "../../../../public/admin/js/custom/widgets";
+import chat from "../../../../public/admin/js/custom/apps/chat/chat";
+import upgradeplan from "../../../../public/admin/js/custom/utilities/modals/upgrade-plan";
+import createapp from "../../../../public/admin/js/custom/utilities/modals/create-app";
+import userssearch from "../../../../public/admin/js/custom/utilities/modals/users-search";
+
+export default {
+    
+};
+</script>
+
+<style></style>
